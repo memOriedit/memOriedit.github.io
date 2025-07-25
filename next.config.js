5407a0e9b91cf6e729b31959e2a93e7f458c6327
henohenon <66393463+henohenon@users.noreply.github.com>
2025-07-25 15:47:02 +0900

    Simplify next.js configuration by removing environment-based basePath and updating assetPrefix settings

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/memOredit.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/memOredit.github.io/' : '',
+  basePath: '',
+  assetPrefix: './',
 }
 
 module.exports = nextConfig
