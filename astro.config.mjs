5fee8b0d4244e80740e8abf7c0b65b7aee9b06f8
henohenon <66393463+henohenon@users.noreply.github.com>
2025-07-28 22:43:58 +0900

    Set up GitHub Pages deployment workflow, update `.gitignore` for `dist`, and modify Astro config for static hosting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 ﻿import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  site: 'https://memoriedit.github.io'
+  site: 'https://memoriedit.github.io',
+  base: './'
 });
